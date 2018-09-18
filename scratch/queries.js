9bebae27007e1f2b8c963d662fbbca209a8a22f7
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-18 15:05:54 -0400

    Implement create a new note

--- scratch/queries.js
+++ scratch/queries.js
@@ -3,31 +3,31 @@ const { MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 
-// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-//   .then(() => {
-//     const searchTerm = /lady gaga/gi;
-//     let filter = {};
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchTerm = /lady gaga/gi;
+    let filter = {};
 
-//     if (searchTerm) {
-//       filter.title = { $regex: searchTerm };
-//     }
+    if (searchTerm) {
+      filter.title = { $regex: searchTerm };
+    }
 
-//     return Note.find(filter).sort({ updatedAt: 'desc' });
-//   })
-//   .then(results => {
-//     console.log(results);
-//   })
-//   .then(() => {
-//     return mongoose.disconnect();
-//   })
-//   .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-//   });
+    return Note.find(filter).sort({ updatedAt: 'desc' });
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => {
-    const id = '000000000000000000000003';
+    const id = '000000000000000000000005';
 
     return Note.findById(id);
   })
@@ -40,4 +40,24 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
     console.error(err);
+  });
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const newNote = {
+      title: 'New Note', 
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus, mi a pharetra gravida, elit lectus blandit nibh, non venenatis velit felis in enim. Etiam non sodales sem. Nunc quis arcu eget quam tincidunt gravida. Vestibulum laoreet sem ut dignissim malesuada. Aenean congue posuere justo laoreet pretium. Cras sem ligula, sollicitudin non magna non, dictum aliquam neque. Etiam luctus maximus quam, ut efficitur ex. Phasellus eu turpis ornare, varius felis vel, eleifend lacus. Proin scelerisque ligula et nunc cursus lacinia. Phasellus placerat diam et feugiat fringilla.'
+    };
+
+    Note.create(newNote)
+      .then(results => {
+        console.log(results);
+      })
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+      });
   });
