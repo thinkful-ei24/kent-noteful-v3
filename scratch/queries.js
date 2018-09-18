4d1ae88040cc0fba99f3d483006059f5788fde7c
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-18 14:52:45 -0400

    Create queries in a scratch file for .find

---
+++ scratch/queries.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchTerm = /lady gaga/gi;
+    let filter = {};
+
+    if (searchTerm) {
+      filter.title = { $regex: searchTerm };
+    }
+
+    return Note.find(filter).sort({ updatedAt: 'desc' });
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
