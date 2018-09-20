bf2cb54b9eaca8a2882ed422d2b3ada47a423e1d
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-20 15:00:04 -0400

    Create folder model and seed

--- utils/seed-databse.js
+++ utils/seed-databse.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
+const Folder = require('../models/folder');
 const Note = require('../models/note');
 
+const { folders } = require('../db/seed/folders');
 const { notes } = require('../db/seed/notes');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(notes))
-  .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Folder.createIndexes
+    ]);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
