383882575503b07f647936b2b29778691eb6f837
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-25 10:33:27 -0400

    Add /refresh endpoint to /routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 const options = { session: false, failWithError: true };
 
 const localAuth = passport.authenticate('local', options);
+const jwtAuth = passport.authenticate('jwt', options);
 
 function createAuthToken(user) {
   return jwt.sign({ user }, JWT_SECRET, {
@@ -18,6 +19,11 @@ function createAuthToken(user) {
 }
 
 router.post('/login', localAuth, function(req, res) {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
+});
+
+router.post('/refresh', jwtAuth, function(req, res) {
   const authToken = createAuthToken(req.user);
   res.json({ authToken });
 });
